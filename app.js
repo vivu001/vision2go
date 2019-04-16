84075e57ed9fbeafb9d252c59909811895255d83
Duc Vu <viet.vu001@stud.fh-dortmund.de>
2019-04-16 19:48:29 +0200

    add partials

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(5000, () => {
+    console.log("Server ist running on port 5000");
+});
