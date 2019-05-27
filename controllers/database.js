212625acb554b8aa34995fb5329421f02d3546fe
vivu001 <43269393+vivu001@users.noreply.github.com>
2019-05-27 17:37:47 +0200

    Update database.js

--- controllers/database.js
+++ controllers/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     // 'url' : 'mongodb://localhost/DoodleClone'
-    'url' : 'mongodb://localhost/vision2go'
-    // 'url' : 'mongodb+srv://vision2go:fb4swt2@v2ocluster-lfznh.mongodb.net/dortmundTravel'
+    // 'url' : 'mongodb://localhost/vision2go'
+    'url' : 'mongodb+srv://vision2go:fb4swt2@v2ocluster-lfznh.mongodb.net/dortmundTravel'
 };
