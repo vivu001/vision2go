2f8fc39317ac1fd8baa5fc3cd5cbd469a8ad8b23
vivu001 <viet.vu001@stud.fh-dortmund.de>
2019-06-23 21:28:37 +0200

    add show/hide search bar

---
+++ controllers/searchBar.js
@@ -0,0 +1,9 @@
+
+function showSearchBar() {
+    let x = document.getElementById("searchBar");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
